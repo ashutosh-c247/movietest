ed3d9a5b96e4e7ead679718e71b3a773a627442b
Ashutosh <ashutosh.s@chapter247.com>
2023-12-27 16:44:49 +0530

    Removed commented code and unused api routes.

--- src/server/router/context.js
+++ src/server/router/context.js
@@ -1,17 +1,9 @@
-// src/server/router/context.ts
 import * as trpc from "@trpc/server";
-// import { getServerSession } from "next-auth";
-
-// import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import prisma from "../db/client";
 
 export const createContext = async (opts) => {
   const req = opts?.req;
   const res = opts?.res;
-
-  //   const options = nextAuthOptions(req);
-
-  //   const session = req && res && (await getServerSession(req, res, options));
 
   return {
     req,
